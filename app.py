4a2bd9eb7a0b5f210ccdd050a080cd3fe8cb8af6
Pasindu Geethanjana <pasinduonlineacc1@gmail.com>
2024-12-14 14:20:25 +0530

    changed greeting message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return '<h1> Flask app kk </h1>'
+    return '<h1> Flask app kk 2.0</h1>'
 
 if __name__ == '__main__':
     app.run()
